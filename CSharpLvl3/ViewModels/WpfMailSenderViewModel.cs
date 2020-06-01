2915e48d03cc946e1b0ae6e08a36f93ebcac25bb
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-01 16:39:43 +0300

    Добавил команду закрытия приложения

---
+++ CSharpLvl3/ViewModels/WpfMailSenderViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using CSharpLvl3.Infrastructure.Commands;
+
+namespace CSharpLvl3.ViewModels
+{
+    internal class WpfMailSenderViewModel
+    {
+        #region Команды
+
+        #region CloseApplicationCommand
+        /// <summary>Команда закрывающая приложение</summary>
+        public ICommand CloseApplicationCommnd { get; }
+
+        private bool CanCloseApplicationCommndExecute(object p) => true;
+
+        /// <summary>Запуск в момент когда команда выпоняется</summary>
+        /// <param name="p"></param>
+        private void OnCloseApplicationCommndExecuted(object p)
+        {
+            Application.Current.Shutdown();
+        }
+        #endregion
+
+        #endregion
+
+        public WpfMailSenderViewModel()
+        {
+            #region Команды
+
+            CloseApplicationCommnd = new LambdaCommand(OnCloseApplicationCommndExecuted, CanCloseApplicationCommndExecute);
+
+            #endregion
+        }
+    }
+}
