489973b5d042c3a5d2510143ff099bdc4dd82d43
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-01 22:44:20 +0300

    Добавил SendEndWindow в тестовое приложение WPF

---
+++ testWPF/SendEndWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace testWPF
+{
+    /// <summary>Логика взаимодействия для SendEndWindow.xaml</summary>
+    public partial class SendEndWindow : Window
+    {
+        public SendEndWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
