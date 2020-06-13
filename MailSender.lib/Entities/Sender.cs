4e748530b41bcb54f1db29bfc1a2d7e012a8ef33
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-13 13:19:41 +0300

    Добавил сущности сервера и отправителя почты

---
+++ MailSender.lib/Entities/Sender.cs
@@ -0,0 +1,13 @@
+﻿
+namespace MailSender.lib.Entities
+{
+    /// <summary>Отправитель письма</summary>
+    public class Sender
+    {
+        /// <summary>Имя отправителя</summary>
+        public string Name { get; set; }
+
+        /// <summary>e-mail отправителя</summary>
+        public string Adress { get; set; }
+    }
+}
---
+++ MailSender.lib/Entities/Server.cs
@@ -0,0 +1,24 @@
+﻿namespace MailSender.lib.Entityes
+{
+    /// <summary>Почтовый сервер</summary>
+    public class Server
+    {
+        /// <summary>Название сервера</summary>
+        public string Name { get; set; }
+
+        /// <summary>Адрес сервера</summary>
+        public string Adress { get; set; }
+
+        /// <summary>Порт сервера</summary>
+        public int Port { get; set; }
+
+        /// <summary>Использование SSL соединения</summary>
+        public bool UseSSL { get; set; }
+
+        /// <summary>Логин отправителя</summary>
+        public string Login { get; set; }
+
+        /// <summary>Пароль отправителя</summary>
+        public string Password { get; set; }
+    }
+}
