a91931f349f3b50412c18b54af3c145a6e817794
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-13 23:02:32 +0300

    Отладочный отправитель почты

--- MailSender.lib/Services/MailSender.cs
+++ MailSender.lib/Services/MailSender.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Net.Mail;
 
 namespace MailSender.lib.Services
@@ -31,6 +32,32 @@ namespace MailSender.lib.Services
                 using (var client = new SmtpClient(_ServerAdress, _Port) { EnableSsl = _UseSSL, Credentials = login })
                     client.Send(message);
             }
+        }
+    }
+
+
+    public class DebugMailSender
+    {
+        private readonly string _ServerAdress;
+        private readonly int _Port;
+        private bool _UseSSL;
+        private string _Login;
+        private string _Password;
+
+        public DebugMailSender(string ServerAdress, int Port, bool UseSSL, string Login, string Password)
+        {
+            _ServerAdress = ServerAdress;
+            _Port = Port;
+            _UseSSL = UseSSL;
+            _Login = Login;
+            _Password = Password;
+        }
+
+        public void Send(string Subject, string Body, string From, string To)
+        {
+            Debug.WriteLine("Отправка почты от {0} к {1} через {2}:{3}[{4}]\r\n{5}:{6}",
+                From, To, _Port, _UseSSL ? "SSL" : "noSSL",
+                Subject, Body);
         }
     }
 }
