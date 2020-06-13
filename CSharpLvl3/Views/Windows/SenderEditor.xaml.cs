5fff4dcde54008e7a2c8bcd541b05744070bdbfe
GeoCod <artem.g.vasilchenko@gmail.com>
2020-06-13 23:31:33 +0300

    Доп. окно редактирования отправителей

---
+++ CSharpLvl3/Views/Windows/SenderEditor.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MailSender.lib.Entities;
+using System.Windows;
+
+namespace CSharpLvl3
+{
+    /// <summary>Логика взаимодействия для SenderEditor.xaml</summary>
+    public partial class SenderEditor
+    {
+        public SenderEditor(Sender Sender)
+        {
+            InitializeComponent();
+            DataContext = Sender;
+        }
+    }
+}
