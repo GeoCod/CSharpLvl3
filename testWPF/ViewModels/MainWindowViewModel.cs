fdac40805b442eb5033e2eccecd46c2cf3015770
GeoCod <artem.g.vasilchenko@gmail.com>
2020-07-04 09:36:25 +0300

    Тестирование привязок свойств

---
+++ testWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,34 @@
+﻿using MailSender.lib.MVVM;
+
+namespace testWPF.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _Title = "Наше новое окно";
+
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value); //Вариант 3
+
+            // Вариант 1
+            //set => _Title = value; // В таком случае нет обратного информирования о изменении свойства
+
+            // Вариант 2 - что бы не дублировать код этот метод реализован в библиотеке
+            //set
+            //{
+            //    if (Equals(_Title, value)) return;
+            //    _Title = value;
+            //    OnPropertyChanged();
+            //}
+        }
+
+        private string _TestValue = "90";
+
+        public string TestValue
+        {
+            get => _TestValue;
+            set => Set(ref _TestValue, value);
+        }
+    }
+}
