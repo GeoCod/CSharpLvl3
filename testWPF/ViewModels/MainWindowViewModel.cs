dd662473d5536914c1e2224724c9744556065253
GeoCod <artem.g.vasilchenko@gmail.com>
2020-07-04 09:50:35 +0300

    Тестирование привязок 2

--- testWPF/ViewModels/MainWindowViewModel.cs
+++ testWPF/ViewModels/MainWindowViewModel.cs
@@ -9,7 +9,13 @@ namespace testWPF.ViewModels
         public string Title
         {
             get => _Title;
-            set => Set(ref _Title, value); //Вариант 3
+            //set => Set(ref _Title, value); //Вариант 3 - основной
+
+            set
+            {
+                if(Set(ref _Title, value))
+                    OnPropertyChanged(nameof(TitleLength));
+            }
 
             // Вариант 1
             //set => _Title = value; // В таком случае нет обратного информирования о изменении свойства
@@ -23,12 +29,29 @@ namespace testWPF.ViewModels
             //}
         }
 
+        public int TitleLength => _Title.Length;
+
         private string _TestValue = "90";
 
         public string TestValue
         {
             get => _TestValue;
             set => Set(ref _TestValue, value);
+        }
+
+
+        private double _X;
+        private double _Y;
+
+        public double X
+        {
+            get => _X;
+            set => Set(ref _X, value);
+        }
+        public double Y
+        {
+            get => _Y;
+            set => Set(ref _Y, value);
         }
     }
 }
