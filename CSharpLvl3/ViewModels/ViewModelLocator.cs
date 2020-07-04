1c5d60248d058b1bd7a6062f6b93c5d10e0cb9a8
GeoCod <artem.g.vasilchenko@gmail.com>
2020-07-04 11:14:35 +0300

    Подключение базовой инфраструктуры MVVM Light

---
+++ CSharpLvl3/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace CSharpLvl3.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            ////if (ViewModelBase.IsInDesignModeStatic)
+            ////{
+            ////    // Create design time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
+            ////}
+            ////else
+            ////{
+            ////    // Create run time view services and models
+            ////    SimpleIoc.Default.Register<IDataService, DataService>();
+            ////}
+
+            //SimpleIoc.Default.Register<MainViewModel>();
+        }
+    }
+}
